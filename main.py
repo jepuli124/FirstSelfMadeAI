69221b534ad9513e43920f5664a9f82156a2fc12
jepuli124 <konsta.jalkanen.kj@gmail.com>
2024-06-12 10:54:16 +0300

    Setup enviroment

---
+++ main.py
@@ -0,0 +1,17 @@
+import gym
+import gym
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from collections import deque
+
+
+import AI
+import enviroment
+
+
+
+
+
